3119874d1b1be5456ee043dc09b7e19b3c0997e1
Katie Gengler <katie@kmg.io>
2017-12-19 23:21:57 -0500

    Convert testem.dist.json to JS and copy pasta that reporter

--- bin/run-browserstack-tests.js
+++ bin/run-browserstack-tests.js
@@ -43,7 +43,7 @@ RSVP.resolve()
     // Calling testem directly here instead of `ember test` so that
     // we do not have to do a double build (by the time this is run
     // we have already ran `ember build`).
-    return run('./node_modules/.bin/testem', [ 'ci', '-f', 'testem.dist.json', '--port', '7000' ]);
+    return run('./node_modules/.bin/testem', [ 'ci', '-f', 'testem.dist.js', '--port', '7000' ]);
   }).then(function() {
     if (process.env.TRAVIS_JOB_NUMBER) {
       return run('./node_modules/.bin/ember', ['browserstack:results']);
---
+++ testem.dist.js
@@ -0,0 +1,70 @@
+var TapReporter = require('testem/lib/reporters/tap_reporter');
+
+function FailureOnlyReporter() {
+  TapReporter.apply(this, arguments);
+  this._reportCount = 0;
+}
+
+FailureOnlyReporter.prototype = Object.create(TapReporter.prototype);
+FailureOnlyReporter.prototype.constructor = FailureOnlyReporter;
+
+FailureOnlyReporter.prototype.display = function(prefix, result) {
+  this._reportCount++;
+
+  if (!result.passed) {
+    TapReporter.prototype.display.apply(this, arguments);
+  }
+
+  if (this._reportCount > 100) {
+    this.out.write('pass count: ' + this.pass);
+    this._reportCount = 0;
+  }
+};
+
+module.exports = {
+  framework: "qunit",
+  test_page: "dist/tests/index.html?hidepassed&hideskipped&timeout=60000",
+  timeout: 540,
+  reporter: new FailureOnlyReporter(),
+  browser_start_timeout: 400,
+  parallel: 4,
+  disable_watching: true,
+  launchers: {
+    BS_Chrome_Current: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "Windows", "--osv", "10", "--b", "chrome", "--bv", "latest", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_Firefox_Current: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "Windows", "--osv", "10", "--b", "firefox", "--bv", "latest", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_Safari_Current: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "OS X", "--osv", "High Sierra", "--b", "safari", "--bv", "11", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_Safari_Last: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "OS X", "--osv", "Sierra", "--b", "safari", "--bv", "10.1", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_MS_Edge: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "Windows", "--osv", "10", "--b", "edge", "--bv", "latest", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_IE_11: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "Windows", "--osv", "10", "--b", "ie", "--bv", "11.0", "--u", "<url>"],
+      protocol: "browser"
+    }
+  },
+  launch_in_dev: [],
+  launch_in_ci: [
+    "BS_Safari_Current",
+    "BS_MS_Edge",
+    "BS_IE_11"
+  ]
+};
